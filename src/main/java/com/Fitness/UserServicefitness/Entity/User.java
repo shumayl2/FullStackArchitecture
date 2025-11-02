2696d7f6113470d16d238fe7db0f40f8c27b341b
Shumayl <shumailislam856@gmail.com>
2025-11-02 12:01:47 +0530

    New Changes

--- src/main/java/com/Fitness/UserServicefitness/Entity/User.java
+++ src/main/java/com/Fitness/UserServicefitness/Entity/User.java
@@ -2,42 +2,31 @@ package com.Fitness.UserServicefitness.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name ="users")
+@Data
 public class User {
 
     @Id
     private String id;
 
 
-    private String name;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private UserRole user = UserRole.USER;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    public User(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
---
+++ src/main/java/com/Fitness/UserServicefitness/Entity/UserRole.java
@@ -0,0 +1,5 @@
+package com.Fitness.UserServicefitness.Entity;
+
+public enum UserRole {
+    USER,ADMIN
+}
